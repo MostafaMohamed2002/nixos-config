718df4b866c84f68fbec13130a710b89087f006d
Mostafa <Mostafa0Mohamed2002@gmail.com>
2026-04-24 11:39:46 +0300

    Add xdg-desktop-portal-hyprland systemd service to graphical-session.target

--- modules/home/systemd.nix
+++ modules/home/systemd.nix
@@ -3,24 +3,38 @@
   pkgs,
   lib,
   ...
-}: {
+}:
+{
   systemd.user.targets."tray" = {
     Unit = {
       Description = "Home Manager System Tray";
-      Requires = ["graphical-session-pre.target"];
+      Requires = [ "graphical-session-pre.target" ];
     };
   };
 
   systemd.user.services.xdg-desktop-portal = {
     Unit = {
       Description = "XDG Desktop Portal";
-      PartOf = ["graphical-session.target"];
+      PartOf = [ "graphical-session.target" ];
     };
     Service = {
       Type = "dbus";
       BusName = "org.freedesktop.portal.Desktop";
       ExecStart = "${lib.getBin pkgs.xdg-desktop-portal}/libexec/xdg-desktop-portal";
     };
-    Install.WantedBy = ["graphical-session.target"];
+    Install.WantedBy = [ "graphical-session.target" ];
+  };
+
+  systemd.user.services.xdg-desktop-portal-hyprland = {
+    Unit = {
+      Description = "XDG Desktop Portal (Hyprland)";
+      PartOf = [ "graphical-session.target" ];
+    };
+    Service = {
+      Type = "dbus";
+      BusName = "org.freedesktop.impl.portal.desktop.hyprland";
+      ExecStart = "${lib.getBin pkgs.xdg-desktop-portal-hyprland}/libexec/xdg-desktop-portal-hyprland";
+    };
+    Install.WantedBy = [ "graphical-session.target" ];
   };
 }
